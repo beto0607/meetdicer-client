e1607c9d13705981226fbec04b997bd994b4181a
Roberto Albanesi <beto0607@gmail.com>
2024-12-05 19:03:01 +0100

    Add search to topbar

--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatInput, MatPrefix } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-topbar',
-  imports: [MatIcon, RouterLink],
+  imports: [MatIcon, RouterLink, MatInput, MatFormField, MatPrefix],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
